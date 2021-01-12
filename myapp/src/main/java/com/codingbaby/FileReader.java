1ac3cbef95cadf8bc595517737f828c9bfaae5b3
vsmysee <vsmysee@gmail.com>
2021-01-12 21:11:55 +0800

    Update FileReader.java

--- myapp/src/main/java/com/codingbaby/FileReader.java
+++ myapp/src/main/java/com/codingbaby/FileReader.java
@@ -7,7 +7,9 @@ import com.github.promeg.pinyinhelper.Pinyin;
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class FileReader {
 
@@ -122,5 +124,23 @@ public class FileReader {
 
         return list;
     }
+
+    public static Map<Character, List<String>> loadEnglishWord(AssetManager assets) {
+        // load english word
+        Map<Character, List<String>> english = new HashMap<>();
+        for (char ch = 'A'; ch <= 'Z'; ch++) {
+            english.put(ch, new ArrayList<String>());
+            try (BufferedReader bf = new BufferedReader(new InputStreamReader(assets.open("cet4/" + ch + ".md")))) {
+                String line;
+                while ((line = bf.readLine()) != null) {
+                    english.get(ch).add(line);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+        return english;
+    }
+
 
 }
